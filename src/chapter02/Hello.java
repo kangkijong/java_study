e351e02b0b2eea8a3384af0bd4619b7e05a4a278
kang <kijong4309@naver.com>
2025-06-24 16:44:19 +0900

    .md modified commit

---
+++ src/chapter02/Hello.java
@@ -0,0 +1,9 @@
+package chapter02;
+
+public class Hello {
+	//main method
+	public static void main(String[] args) {
+		System.out.print("Welcome to JAVA!!");
+		System.out.println("Hello~");
+	}
+}
