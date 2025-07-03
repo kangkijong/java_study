8f0294b0f07a9f06087ca3f67a2c2b22d7414e1b
kang <kijong4309@naver.com>
2025-07-03 17:42:09 +0900

    2025-07-03 commit

--- src/chapter07/ScoreMgmSystem.java
+++ src/chapter07/ScoreMgmSystem.java
@@ -7,10 +7,11 @@ import java.util.Scanner;
  */
 public class ScoreMgmSystem {
 	// Field
+	Scanner scan = new Scanner(System.in);
 	String admin;
 	Student student;
-	Scanner scan = new Scanner(System.in);
 	Student[] slist = new Student[3];	// 배열로 학생 10명의 정보를 받는다면
+	int count = 0;
 	
 	// Constructor
 	public ScoreMgmSystem() {}
@@ -18,33 +19,59 @@ public class ScoreMgmSystem {
 	// Method
 	public void insert() {
 		System.out.println("-- insert --");
-		student = new Student();
+//		student = new Student();
+
+		for(int i=count ; i<slist.length ; i++) {
+			slist[i] = new Student();
+			
+			System.out.print("학생명> ");
+			slist[i].name = scan.next();
+			
+			System.out.print("국어> ");
+			slist[i].kor = scan.nextInt();
+			
+			System.out.print("영어> ");
+			slist[i].eng = scan.nextInt();
+			
+			System.out.print("수학> ");
+			slist[i].math = scan.nextInt();
+			
+			count++;
+			
+			System.out.print("계속 입력 하시겠습니까?(계속:아무키나 누르세요, 종료:n)> ");
+			if(scan.next().equals("n")) {
+				System.out.println("=> 등록 완료!!");
+				break;	// break;
+			}
+		}
 		
-		System.out.print("학생명> ");
-		student.name = scan.next();
-		
-		System.out.print("국어> ");
-		student.kor = scan.nextInt();
-		
-		System.out.print("영어> ");
-		student.eng = scan.nextInt();
-		
-		System.out.print("수학> ");
-		student.math = scan.nextInt();
-		
-		System.out.println("=> 등록 완료!!");
+//		System.out.print("학생명> ");
+//		student.name = scan.next();
+//		
+//		System.out.print("국어> ");
+//		student.kor = scan.nextInt();
+//		
+//		System.out.print("영어> ");
+//		student.eng = scan.nextInt();
+//		
+//		System.out.print("수학> ");
+//		student.math = scan.nextInt();
+//		
+//		System.out.println("=> 등록 완료!!");
 	}
 	
 	public void show() {
 		System.out.println("-----------------------------------------------");
 		System.out.println("학생명\t국어\t영어\t수학\t총점\t평균");
 		System.out.println("-----------------------------------------------");
-		System.out.print(student.name+"\t");
-		System.out.print(student.kor+"\t");
-		System.out.print(student.eng+"\t");
-		System.out.print(student.math+"\t");
-		System.out.print(student.getTot()+"\t");
-		System.out.println(student.getAvg());
+		for(int i=0 ; i<count ; i++) {
+			System.out.print(slist[i].name+"\t");
+			System.out.print(slist[i].kor+"\t");
+			System.out.print(slist[i].eng+"\t");
+			System.out.print(slist[i].math+"\t");
+			System.out.print(slist[i].getTot()+"\t");
+			System.out.println(slist[i].getAvg());
+		}
 		System.out.println("----------------------------------------------");
 	}
 	
